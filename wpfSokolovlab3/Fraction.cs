b0cab5c922025dba9d7ac8a2554901e62fa58175
dobry <dobry@DESKTOP-FBD3PAQ>
2024-02-08 21:38:41 +0300

    Добавьте файлы проекта.

---
+++ wpfSokolovlab3/Fraction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfSokolovlab3
+{
+    class Fraction
+    {
+
+
+        public long First { get; set; }
+        public ushort Second { get; set; }
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 + a2, 3);
+            return new Fraction() { First = (long)Math.Floor(res), Second = ushort.Parse(res.ToString().Split(",")[1]) };
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f2.Second.ToString().Length));
+            double res = Math.Round(a1 - a2, 3);
+
+            return new Fraction() { First = (long)Math.Floor(res), Second = ushort.Parse(res.ToString().Split(',')[1]) };
+
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 * a2, 3);
+            return new Fraction() { First = (long)Math.Floor(res), Second = ushort.Parse(res.ToString().Split(",")[1]) };
+        }
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f2.Second.ToString().Length));
+            double res = Math.Round(a1 / a2, 3);
+            return new Fraction() { First = (long)Math.Floor(res), Second = ushort.Parse(res.ToString().Split(",")[1]) };
+        }
+
+
+        public override string? ToString()
+        {
+            return First + "," + Second;
+        }
+
+    }
+}
+
+
---
+++ wpfSokolovlab3/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfSokolovlab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void but1_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction f1 = new Fraction()
+            {
+                First = long.Parse(b1.Text),
+                Second = ushort.Parse(b3.Text)
+            };
+            Fraction f2 = new Fraction()
+            {
+                First = long.Parse(b2.Text),
+                Second = ushort.Parse(b4.Text)
+            };
+            Fraction res = f1 + f2;
+            Res.Text = res.ToString();
+        }
+
+        private void but2_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction f1 = new Fraction()
+            {
+                First = long.Parse(b1.Text),
+                Second = ushort.Parse(b3.Text)
+            };
+            Fraction f2 = new Fraction()
+            {
+                First = long.Parse(b2.Text),
+                Second = ushort.Parse(b4.Text)
+            };
+            Fraction res = f1 - f2;
+            Res.Text = res.ToString();
+        }
+
+        private void but3_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction f1 = new Fraction()
+            {
+                First = long.Parse(b1.Text),
+                Second = ushort.Parse(b3.Text)
+            };
+            Fraction f2 = new Fraction()
+            {
+                First = long.Parse(b2.Text),
+                Second = ushort.Parse(b4.Text)
+            };
+            Fraction res = f1 * f2;
+            Res.Text = res.ToString();
+        }
+
+        private void but4_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction f1 = new Fraction()
+            {
+                First = long.Parse(b1.Text),
+                Second = ushort.Parse(b3.Text)
+            };
+            Fraction f2 = new Fraction()
+            {
+                First = long.Parse(b2.Text),
+                Second = ushort.Parse(b4.Text)
+            };
+
+            Fraction res = f1 / f2;
+            Res.Text = res.ToString();
+
+
+        }
+        private void but5_Click(object sender, RoutedEventArgs e)
+        {
+            Fraction f1 = new Fraction()
+            {
+                First = long.Parse(b1.Text),
+                Second = ushort.Parse(b3.Text)
+            };
+            Fraction f2 = new Fraction()
+            {
+                First = long.Parse(b2.Text),
+                Second = ushort.Parse(b4.Text)
+            };
+            if (f1.First == f2.First && f1.Second == f2.Second)
+            {
+                Res.Text = "они равны".ToString();
+            }
+            else Res.Text = "они не равны".ToString();
+        }
+    }
+}
